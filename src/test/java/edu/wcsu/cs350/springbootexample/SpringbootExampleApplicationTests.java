3983498583872c297669f097a61ec1ede279e8ff
Lucas Vas <lvas463@gmail.com>
2024-11-21 09:33:05 -0500

    Initial commit

---
+++ src/test/java/edu/wcsu/cs350/springbootexample/SpringbootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.wcsu.cs350.springbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
